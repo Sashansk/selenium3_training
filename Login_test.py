afd054ceaa9ee90bfbdbaa5bc43236f5ace28ed0
Sashansk <zuikof@bk.ru>
2022-11-24 17:20:15 +0700

    New_login_test

--- Login_test.py
+++ Login_test.py
@@ -11,10 +11,11 @@ def wdrv(request):
     return wd
 
 
-def test_example(wdrv, uname="admin", pwd="admin"):
+def test_example(wdrv, name="admin", pwd="admin"):
     wdrv.get("http://localhost/litecart/admin/")
-    wdrv.find_element("name", "username").send_keys(uname)
+    wdrv.find_element("name", "username").send_keys(name)
     wdrv.find_element("name", "password").send_keys(pwd)
+    time.sleep(1)
     wdrv.find_element("name", "login").click()
     time.sleep(1)
     wdrv.find_element(By.CLASS_NAME, "fa-sign-out").click()
