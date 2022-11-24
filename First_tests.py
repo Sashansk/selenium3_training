dd9c101ea83f56ceedd5496c5439ad136426f0fa
Sashansk <zuikof@bk.ru>
2022-11-24 16:55:34 +0700

    logintest

---
+++ First_tests.py
@@ -0,0 +1,20 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+
+@pytest.fixture
+def wdrv(request):
+    wd = webdriver.Chrome()
+    request.addfinalizer(wd.quit)
+    return wd
+
+
+def test_example(wdrv, uname="admin", pwd="admin"):
+    wdrv.get("http://localhost/litecart/admin/")
+    wdrv.find_element("name", "username").send_keys(uname)
+    wdrv.find_element("name", "password").send_keys(pwd)
+    wdrv.find_element("name", "login").click()
+    time.sleep(1)
+    wdrv.find_element(By.CLASS_NAME, "fa-sign-out").click()
