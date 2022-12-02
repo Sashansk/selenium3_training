fcc72e6f8b48ebe8813e390c065b9f0fc0782f40
Sashansk <zuikof@bk.ru>
2022-12-02 19:34:06 +0700

    6 task пока только схема

--- test/6task_adminka_test.py
+++ test/6task_adminka_test.py
@@ -10,21 +10,14 @@ def test_adminka (wd):
     time.sleep(1)
     wd.find_element("name", "login").click()
     time.sleep(1)
-    menu = wd.find_elements(By.CSS_SELECTOR, "#app- span.name:nth-child(1)")
-    list = menu.wd.find_element(By.CSS_SELECTOR, "#doc-template > a > span")
-    list.find_element(By.TAG_NAME, "h1")
-    wd.find_element(By.CSS_SELECTOR, "#doc-logotype > a > span").click()
-    wd.find_element(By.TAG_NAME, "h1")
-    wd.find_element(By.CSS_SELECTOR, ("#app- > a > span.name")[1]).click()
-    time.sleep(3)
-    wd.find_element(By.CSS_SELECTOR, "#doc-catalog > a > span").click()
-    wd.find_element(By.TAG_NAME, "h1")
-    wd.find_element(By.CSS_SELECTOR, "#doc-product_groups > a > span").click()
-    wd.find_element(By.TAG_NAME, "h1")
-    #p = page.find.element(By.CSS_SELECTOR = "#content > h1").
-    #a = elem.find_element()
-    time.sleep(4)
-    #    wd.find_element("id", "doc-template").click()
-    # wd.find_element("id", "doc-logotype").click()
-
-
+    i = 0
+    j = 0
+    while i < len(menu):
+        menu = wd.find_elements(By.CSS_SELECTOR, "#app- span.name:nth-child(1)") #Нашли все элементы меню
+        menu[i].click() #зашли в меню
+        while j < len(list):
+            list[j] = menu[i].wd.find_elements(By.CSS_SELECTOR, "#doc-template (тут должен вернуться список подменю) > a > span:nth-child(1)") #выбрали элементы подменю
+            list[j].click() # перешли на выбранную страницу подменю
+            list[j].find_element(By.TAG_NAME, "h1") # находим тег с заголовком
+            j += 1
+        i += 1
